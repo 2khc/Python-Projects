f0fa86513a204dbb88dc4f3813e866088659b864
2khc <kh.kelvinchan@gmail.com>
2016-06-10 17:36:26 +0100

    finished

--- Project Euler/1/main.py
+++ Project Euler/1/main.py
@@ -1,1 +1,13 @@
-print "hi"
+# Project Euler Problem 1 - Multiples of 3 and 5
+# =============================================================
+
+i = 0
+sum = 0
+
+while i < 1000:
+    if i % 3 == 0 or i % 5 == 0:
+        sum += i
+    i += 1
+
+print "Sum of multiples of 3 and 5 for numbers below 1000 is:\n" \
+      "%d\n" % sum
