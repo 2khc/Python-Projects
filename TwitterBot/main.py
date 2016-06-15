56f63a131b82aae71d7385e4696aaa826886ce09
2khc <kh.kelvinchan@gmail.com>
2016-06-15 19:13:44 +0100

    twitterApi.y added

--- TwitterBot/main.py
+++ TwitterBot/main.py
@@ -7,14 +7,17 @@ import sys
 import json
 import random
 import twitter
+import twitterApi
 
 consumer_token = "6IzigtciQ0wQPhfKQK5a57YWC"
 consumer_secret = "qY5b9h3cwY39EYxW6zKIpWteElfTL32pPXYQeg8hqvpzYX639U"
 access_token = "2304246030-2V2MbJVRNUXNQB3ixPi0aIZ3YUJx88UhKS3HYxH"
 access_token_secret = "ZtGQKu1rUnDFBKytdeu4j0RFthoOS6cO2BSuTXZTwA2zI"
 
-api = twitter.Api(consumer_key=consumer_token, consumer_secret=consumer_secret, access_token_key=access_token,
-                  access_token_secret=access_token_secret)
+# api = twitter.Api(consumer_key=consumer_token, consumer_secret=consumer_secret, access_token_key=access_token,
+#access_token_secret = access_token_secret)
+
+api = twitterApi.set_twitter_api(consumer_token, consumer_secret, access_token, access_token_secret)
 
 
 def connect_psql():
@@ -40,108 +43,6 @@ def connect_psql():
 # public_tweets = api.home_timeline()
 # for tweets in public_tweets:
 #     print tweets
-
-# Search for a user, returns an array of user screen names
-# 1 get query
-def search_term(term):
-    results = api.GetSearch(term)
-    users = []
-    a = results[0]
-    for user in results:
-        users.append(user.user.screen_name)
-    return users
-
-
-# Looks up the friendship that you have with a particular user of choice
-# 1 get query
-def lookup_friendship(screen_name):
-    status = api.LookupFriendship(screen_name=screen_name)
-    connections = status[0].connections
-    return connections
-
-
-# Get a specified User model
-# 1 get query
-def search_user(user):
-    userModel = api.GetUser(screen_name=user)
-    print userModel
-
-
-# Follows a user, write the user into a json file
-def follow_user(json_file, screen_name, is_followed_by):
-    follow_date = time.time()
-    object = {screen_name: {"following": True,
-                            "followed_by": is_followed_by,
-                            "follow_date": follow_date}}
-    api.CreateFriendship(screen_name=screen_name)
-    try:
-        with open(json_file) as f:
-            data = json.load(f)
-
-        data.update(object)
-
-        with open(json_file, 'w') as f:
-            json.dump(data, f)
-    except:
-        with open(json_file, 'w') as f:
-            json.dump(object, f)
-
-
-# Add a search term to the search_terms.txt file
-def add_search_term(search_term):
-    try:
-        with open('search_terms.txt', 'r+') as f:
-            term_exists = False
-            for line in f:
-                print line
-                if line == search_term + '\n':
-                    term_exists = True
-            if not term_exists:
-                f.write(search_term + '\n')
-    except:
-        with open('search_terms.txt', 'w') as f:
-            f.write(search_term)
-
-
-# Get all search terms in a list
-def get_search_terms(file):
-    data = []
-    with open(file) as f:
-        for line in f:
-            data.append(line.strip())
-    print data
-    return data
-
-
-# Remove a search term from the file
-def remove_search_term():
-    pass
-
-
-def open_file(json_file):
-    with open(json_file) as f:
-        data = json.load(f)
-    return data
-
-
-def mass_follow(search_terms_file, users_file, followed_file, maximum_follow_delay):
-    terms = get_search_terms(search_terms_file)
-    users_json = open_file(users_file)
-    count = 0
-    for term in terms:
-        users = search_term(term)
-        for user in users:
-            if user in users_json:
-                continue
-            else:
-                connections = lookup_friendship(
-                    user)  # of the form {u'muting': False, u'followed_by': False, u'following': False, u'following_received': False, u'blocking': False, u'following_requested': False}
-                follow_user(users_file, user, connections['followed_by'])
-                update_daily_followed(followed_file)
-                print "Followed %s" % user
-                count += 1
-                time.sleep(random.randrange(maximum_follow_delay - 10, maximum_follow_delay))
-    print "Followed %d people." % count
 
 
 def check_daily_followed(file_name):
@@ -196,14 +97,54 @@ def check_rate_limit(url):
     print status
 
 
+# Remove a search term from the file
+def remove_search_term():
+    pass
+
+
+def open_file(json_file):
+    with open(json_file) as f:
+        data = json.load(f)
+    return data
+
+
+# Add a search term to the search_terms.txt file
+def add_search_term(search_term):
+    try:
+        with open('search_terms.txt', 'r+') as f:
+            term_exists = False
+            for line in f:
+                print line
+                if line == search_term + '\n':
+                    term_exists = True
+            if not term_exists:
+                f.write(search_term + '\n')
+    except:
+        with open('search_terms.txt', 'w') as f:
+            f.write(search_term)
+
+
+# Get all search terms in a list
+def get_search_terms(file):
+    data = []
+    with open(file) as f:
+        for line in f:
+            data.append(line.strip())
+    print data
+    return data
+
+
 # Get all followers, if someone is not following back after 2 days
 def main():
-    # json = {"screen_name": "kelv"}
+
+    # Constants.
     search_terms_file = 'search_terms.txt'
     users_file = 'users.json'
     daily_followed_file = 'followed.txt'
     follow_limit = 1000
     max_follow_delay = 70
+
+
     followed = check_daily_followed(daily_followed_file)
     # update_daily_followed(daily_followed_file)
 
@@ -227,17 +168,8 @@ def main():
             add_search_term(search_term)
 
     while followed < 1000:
-        mass_follow(search_terms_file, users_file, daily_followed_file, max_follow_delay)
+        twitterApi.mass_follow(search_terms_file, users_file, daily_followed_file, max_follow_delay)
         followed = update_daily_followed(daily_followed_file)
 
-        # mass_follow(search_terms_file, users_file, max_follow_delay)
-        # while followed < follow_limit:
-        #     pass
-        #
-        # follow_user(users_file, 'elementalstory', False)
-
-
-# user = api.get_user('2khch')
-# print user.followers_count
 
 main()
---
+++ TwitterBot/twitterApi.py
@@ -0,0 +1,73 @@
+import twitter
+import json
+
+
+def set_twitter_api(consumer_token, consumer_secret, access_token, access_token_secret):
+    return twitter.Api(consumer_key=consumer_token, consumer_secret=consumer_secret, access_token_key=access_token,
+                       access_token_secret=access_token_secret)
+
+
+# Search for a user, returns an array of user screen names
+# 1 get query
+def search_term(term):
+    results = api.GetSearch(term)
+    users = []
+    a = results[0]
+    for user in results:
+        users.append(user.user.screen_name)
+    return users
+
+
+# Looks up the friendship that you have with a particular user of choice
+# 1 get query
+def lookup_friendship(screen_name):
+    status = api.LookupFriendship(screen_name=screen_name)
+    connections = status[0].connections
+    return connections
+
+
+# Get a specified User model
+# 1 get query
+def search_user(user):
+    userModel = api.GetUser(screen_name=user)
+    print userModel
+
+
+# Follows a user, write the user into a json file
+def follow_user(json_file, screen_name, is_followed_by):
+    follow_date = time.time()
+    object = {screen_name: {"following": True,
+                            "followed_by": is_followed_by,
+                            "follow_date": follow_date}}
+    api.CreateFriendship(screen_name=screen_name)
+    try:
+        with open(json_file) as f:
+            data = json.load(f)
+
+        data.update(object)
+
+        with open(json_file, 'w') as f:
+            json.dump(data, f)
+    except:
+        with open(json_file, 'w') as f:
+            json.dump(object, f)
+
+
+def mass_follow(search_terms_file, users_file, followed_file, maximum_follow_delay):
+    terms = get_search_terms(search_terms_file)
+    users_json = open_file(users_file)
+    count = 0
+    for term in terms:
+        users = search_term(term)
+        for user in users:
+            if user in users_json:
+                continue
+            else:
+                connections = lookup_friendship(
+                    user)  # of the form {u'muting': False, u'followed_by': False, u'following': False, u'following_received': False, u'blocking': False, u'following_requested': False}
+                follow_user(users_file, user, connections['followed_by'])
+                update_daily_followed(followed_file)
+                print "Followed %s" % user
+                count += 1
+                time.sleep(random.randrange(maximum_follow_delay - 10, maximum_follow_delay))
+    print "Followed %d people." % count
